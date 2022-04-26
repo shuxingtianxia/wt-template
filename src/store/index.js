670eb86cc4ed11f0ac40bf40660e4f540dffdb54
laotongshu <laotongshu@wingtech.com>
2022-04-26 09:55:18 +0800

    feat: 更换store的文件夹

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import getters from './getters'
-import wtadmin from './modules'
 Vue.use(Vuex)
+
+const files = require.context('./modules', false, /\.js$/)
+
+const modules = {}
+files.keys().forEach(key => {
+  modules[key.replace(/(\.\/|\.js)/g, '')] = files(key).default
+})
 export default new Vuex.Store({
-  modules: {
-    ...wtadmin
-  },
+  modules,
   getters
 })
