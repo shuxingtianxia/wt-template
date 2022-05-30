6f5ebfd7df1afa5a053fbbd1684debabd000c72a
laotongshu <laotongshu@wingtech.com>
2022-05-30 11:04:40 +0800

    fix: 开发环境不要sentry

--- src/plugin/sentry/index.js
+++ src/plugin/sentry/index.js
@@ -5,18 +5,20 @@ import { Integrations } from '@sentry/tracing'
 
 import router from '@/router'
 
-Sentry.init({
-  Vue,
-  dsn: 'http://9bc9e3878a894a8bbac9516369b77a58@192.168.56.101:9000/2',
-  integrations: [
-    new Integrations.BrowserTracing({
-      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
-      tracingOrigins: ['localhost', 'my-site-url.com', /^\//]
-    })
-  ],
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-  release: process.env.VUE_APP_RELEASE
-})
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    Vue,
+    dsn: 'http://9bc9e3878a894a8bbac9516369b77a58@192.168.56.101:9000/2',
+    integrations: [
+      new Integrations.BrowserTracing({
+        routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+        tracingOrigins: ['localhost', 'my-site-url.com', /^\//]
+      })
+    ],
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+    release: process.env.VUE_APP_RELEASE
+  })
+}
